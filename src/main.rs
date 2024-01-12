eb32583504954460fcf311e458b89f04849cc593
Stefan Wong <stfnwong@gmail.com>
2024-01-13 01:38:52 +1300

    A Pratt parser with precedence resolution for infix ops

--- src/main.rs
+++ src/main.rs
@@ -73,6 +73,66 @@ impl fmt::Display for S {
 }
 
 
+fn infix_binding_power(op: char) -> (u8, u8)
+{
+    match op {
+        '+' | '-' => (1, 2),
+        '*' | '/' => (3, 4),
+        _ => panic!("bad op {:?}", op)
+    }
+}
+
+// Start with two infix binary operators 
+fn expr(input: &str) -> S 
+{
+    let mut lexer = Lexer::new(input);
+    expr_bp(&mut lexer, 0)
+}
+
+fn expr_bp(lexer: &mut Lexer, min_bp: u8) -> S 
+{
+    let mut lhs = match lexer.next() {
+        Token::Atom(it) => S::Atom(it),
+        t => panic!("bad token {:?}", t),
+    };
+
+    loop {
+        let op = match lexer.peek() {
+            Token::Eof => break,
+            Token::Op(op) => op,
+            t => panic!("bad token {:?}", t),
+        };
+
+        let (left_bp, right_bp) = infix_binding_power(op);
+        if left_bp < min_bp {
+            break;
+        }
+
+        lexer.next();
+        let rhs = expr_bp(lexer, right_bp);
+
+        lhs = S::Cons(op, vec![lhs, rhs]);
+    }
+
+    return lhs;
+}
+
+
+
+
+#[test]
+fn tests() {
+    let s = expr("1");
+    assert_eq!(s.to_string(), "1");
+
+    let s = expr("1 + 2 * 3");
+    assert_eq!(s.to_string(), "(+ 1 (* 2 3))");
+}
+
+// === TESTS ==== //
+// TODO: move to another module?
+
 fn main() {
-    println!("Hello, world!");
+    let s = expr("1 + 2 * 3");
+    println!("{:?}", s.to_string());
 }
